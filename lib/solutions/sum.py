c72f0f7c79a03e4dbcbb044a9b211b4e4103fb14
sbx_user1059 <sbx_user1059@ip-10-11-103-241.eu-west-2.compute.internal>
2018-06-07 16:21:36 +0000

    Thu Jun 07 16:21:36 UTC 2018

--- lib/solutions/sum.py
+++ lib/solutions/sum.py
@@ -3,7 +3,7 @@ def sum(x, y):
     # Check if x and y are integer between 0 and 100
     if not isinstance(x, int) or not isinstance(y, int):
         return None
-    if x > 0 and x < 100 and y > 0 and y < 100:
+    if x >= 0 and x <= 100 and y >= 0 and y <= 100:
         return x + y
     return None
     # raise NotImplementedError()
