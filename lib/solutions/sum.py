47ab0f2b0ea51bfdd1f49b0dbc87eaca0d2c8008
sbx_user1059 <sbx_user1059@ip-10-11-103-241.eu-west-2.compute.internal>
2018-06-07 16:17:39 +0000

    Thu Jun 07 16:17:39 UTC 2018

--- lib/solutions/sum.py
+++ lib/solutions/sum.py
@@ -1,4 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def sum(x, y):
-    
+    # Check if x and y are integer between 0 and 100
+    if not isinstance(x, int) or not isinstance(y, int):
+        return None
+    if 
     raise NotImplementedError()
