ddcc68a50c03a587033fc586ff92a2098123b229
sbx_user1059 <sbx_user1059@ip-10-11-228-255.eu-west-2.compute.internal>
2018-06-08 10:54:09 +0000

    Fri Jun 08 10:54:09 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,4 +1,12 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+
+    if not isinstance(skus, basestring):
+        return -1
+
+    # Divide string in a list of letters
+    skus = list(skus)
+    
+
     raise NotImplementedError()
